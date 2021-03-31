f1ddbeb06eb8b8cfe0306d70fb074bed662613d6
Ryan Simkoff <rsimkoff@gmail.com>
2021-03-30 23:25:28 -0400

    company html added

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,23 +9,24 @@ const headerText = $('#headerText');
 const submitCompanyInfo = $('#submitCompanyInfo');
 
 
-function submitNewCompany()
+async function submitNewCompany()
 {
     //save company values to local storage
-    saveCompanyValues();
+    await saveCompanyValues(true);
     //load company.html
     window.location.href = "\\Group-Project-1\\company.html";
 }
 
-function submitExistingCompany()
+async function submitExistingCompany()
 {
     //update company values in local storage
-    saveCompanyValues();
+    await saveCompanyValues(false);
     //load home html
     window.location.href = "\\Group-Project-1\\home.html";
 }
 
-function saveCompanyValues(){
+async function saveCompanyValues(getImgURL)
+{
     let companyValues = {};
     companyValues.name = companyName.val();
     companyValues.employeeNumber = employeeNumber.val();
@@ -34,6 +35,14 @@ function saveCompanyValues(){
     companyValues.city = companyCity.val();
     companyValues.state = companyState.val();
     companyValues.projectManager = companyProjectManager.val();
+    if(getImgURL)
+    {
+        companyValues.imgURL = await getImage()
+    }else
+    {
+        var companyObj = JSON.parse(localStorage.getItem('companyValues')); //assuming the result of get item is not null
+        companyValues.imgURL = companyObj.imgURL;
+    }
     localStorage.setItem('companyValues', JSON.stringify(companyValues));
 };
 
@@ -60,6 +69,23 @@ function updatePageDetails(header, buttonText)
 
 companyExists();
 
+//Fetch call
+
+async function asyncGetImage(){
+    let requestURL = `https://source.unsplash.com/featured/?construction,roof`;
+    const response = await fetch(requestURL);
+    return await response;
+};
+
+async function getImage()
+{   
+    let imageURL;
+    await asyncGetImage().then(data => {
+        imageURL = data.url.toString();
+    });
+    return imageURL;
+}
+
 
 // Array of objects for project jobs 
 
@@ -70,9 +96,9 @@ const item = [
 { name: 'Barn Storm Repair', client: 'Barney Hyde', cost: '$25,000', description: "Help! I was milking bessie during a catergory 4 hurricane and a lightning bolt startled the old heifer. She ran through the door and tore off the hinges belching like a locomotive. If you can a help a geezer like me, I'll give you some Dogecoin USD.", location: 'Birmingham', time: '1 month', documentUrl: 'https://www.youtube.com/watch?v=hRxOjvHFdo0' },
 { name: 'Urban Apartment complex', client: 'Hercules', cost: '$30,000', description: 'Installing new roofing shingles for apartment complex update.', location: 'Atlanta', time: '2 weeks', documentUrl: '' },
 { name: 'Metropolitan duplex', client: 'Tony Stark', cost: '$15,000', description: 'Roof repair for recent hale storm damage.', location: 'Miami', time: '3 Days', documentUrl: '' },
-{ name: 'Back Deck Construction', client: 'Tony Stark', cost: '$15,000', description: 'Roof repair for recent hale storm damage.', location: 'Miami', time: '3 Days', documentUrl: '' },
-{ name: 'Metropolitan duplex', client: 'Tony Stark', cost: '$15,000', description: 'Roof repair for recent hale storm damage.', location: 'Miami', time: '3 Days', documentUrl: '' },
-{ name: 'Metropolitan duplex', client: 'Tony Stark', cost: '$15,000', description: 'Roof repair for recent hale storm damage.', location: 'Miami', time: '3 Days', documentUrl: '' }
+{ name: 'Back Deck Construction', client: 'Rob Smith', cost: '$8,000', description: 'Splurging on a new back deck with full gaming station, and robot.', location: 'Cumming', time: '1 Day', documentUrl: '' },
+{ name: 'Kitchen Renovation', client: 'Karen Candle', cost: '$10,000', description: 'Updating kitchen with modern ammenities. ', location: 'Duluth', time: '2 weeks', documentUrl: '' },
+{ name: 'Window Installation', client: 'Jim Bean', cost: '$3,000', description: 'Installing windows into a brand new house,looking for a small local exterior company.', location: 'Memphis', time: '3 weeks', documentUrl: '' }
 ]
 // For-of function for selecting an object from array based on search criteria
 // function for (let index = 0; index < array.length; index++) {
@@ -85,11 +111,11 @@ const item = [
 
 //assign 
 
-// function selectProject()
-// {
-//     for (const iterator of item) {
-//         item.filter(res => res.cost.condition > 0).map(ele=>ele.companyValues) 
-       
+function selectProject()
+{
+    for (const value of item) {
+        item.filter(res => res.cost.condition > 0).map(ele=>ele.companyValues) 
+       window.log(value);
         
-//     }
-// }
+    }
+}
